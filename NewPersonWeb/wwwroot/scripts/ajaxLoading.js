44982fb8f8cb2bae6ff9a35e1daf914822b196e0
salar <salar_taremy@yahoo.com>
2020-12-02 22:33:22 +0330

    fff

---
+++ NewPersonWeb/wwwroot/scripts/ajaxLoading.js
@@ -0,0 +1,11 @@
+﻿$body = $("body");
+$(document).on({
+    ajaxStart: function () {
+        $body.addClass("loading");
+        console.log("ajax Start...");
+    },
+    ajaxStop: function () {
+         $body.removeClass("loading");
+        console.log("ajax Stop !");
+    }
+});
