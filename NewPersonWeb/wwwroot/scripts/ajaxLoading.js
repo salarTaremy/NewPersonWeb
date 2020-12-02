baa44f175eb6755283848a1061dcc82ab52175e2
salar <salar_taremy@yahoo.com>
2020-12-02 22:41:30 +0330

    555

--- NewPersonWeb/wwwroot/scripts/ajaxLoading.js
+++ NewPersonWeb/wwwroot/scripts/ajaxLoading.js
@@ -1,11 +1,14 @@
-﻿$body = $("body");
+﻿// you must add this tag in html page
+//  <div class="modal"><!-- For Ajax Loading --></div>
+
+$body = $("body");
 $(document).on({
     ajaxStart: function () {
         $body.addClass("loading");
         console.log("ajax Start...");
     },
     ajaxStop: function () {
-         $body.removeClass("loading");
+        $body.removeClass("loading");
         console.log("ajax Stop !");
     }
 });
